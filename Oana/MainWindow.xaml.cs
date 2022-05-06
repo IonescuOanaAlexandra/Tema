375c51b73aa9f82b19bdcf418ce46be90759222e
IonescuOanaAlexandra <92166106+IonescuOanaAlexandra@users.noreply.github.com>
2022-05-06 10:26:33 +0300

    Add files via upload

---
+++ Oana/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uana
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+                if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text) && cb.IsChecked==true)
+    {
+        lstNames.Items.Add(txtName.Text);
+                eroare.Visibility= Visibility.Hidden;
+        txtName.Clear();
+    }
+            else
+            {
+                eroare.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
